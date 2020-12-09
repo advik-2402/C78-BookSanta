5d047b68e5f5354dd0b303342058af288b2a5d58
Advik <katiyar.advik07@gmail.com>
2020-12-09 15:01:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignUp from './screens/SignUpScreen';
+
+export default function App() {
+  return (
+    <View>
+      <SignUp />
+    </View>
+  );
+}
